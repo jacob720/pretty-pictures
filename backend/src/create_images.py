6428616204e553d32b28ff6c4d6a877d47061de1
jacob720 <jacobwilliamson11@yahoo.co.uk>
2025-12-26 17:45:29 +0000

    Add files

--- backend/src/create_images.py
+++ backend/src/create_images.py
@@ -19,7 +19,7 @@ def get_weights(
     turn_back_r=False,
     turn_back_g=False,
     turn_back_b=False,
-) -> list[int, int, int]:
+) -> dict[str, int]:
     return {
         "r": int(
             r % 256 if ((not turn_back_r) or ((r // 256) % 2 == 0)) else 256 - r % 256
@@ -51,29 +51,50 @@ def get_parameters(n: int, template: Template) -> Generator[dict]:
                 "g": template.level_g,
                 "b": template.level_b,
             },
+            "x_factors": {
+                "r": template.x_factor_r,
+                "g": template.x_factor_g,
+                "b": template.x_factor_b,
+            },
+            "y_factors": {
+                "r": template.y_factor_r,
+                "g": template.y_factor_g,
+                "b": template.y_factor_b,
+            },
         }
         for i in range(n)
     )
     return (image_params for image_params in params if image_params is not None)
 
 
-def create_pattern(width: int, height: int, weights: dict, levels: dict) -> np.ndarray:
+def create_pattern(
+    width: int,
+    height: int,
+    velocitys: dict,
+    levels: dict,
+    x_factors,
+    y_factors,
+    blink: bool = True,
+) -> np.ndarray:
     x = np.arange(width).reshape(-1, 1)
     y = np.arange(height).reshape(1, -1)
 
     r = (
-        ((0.5 * x + y) % weights["r"]) * levels["r"]
-        if weights["r"]
+        ((x_factors["r"] * x + y_factors["r"] * y) % velocitys["r"])
+        * (levels["r"] * ((256 / velocitys["r"]) if not blink else 1))
+        if velocitys["r"]
         else np.zeros_like(x + y)
     )
     g = (
-        ((1 * x + 10 * y) % weights["g"]) * levels["g"]
-        if weights["g"]
+        ((x_factors["g"] * x + y_factors["g"] * y) % velocitys["g"])
+        * (levels["g"] * ((256 / velocitys["g"]) if not blink else 1))
+        if velocitys["g"]
         else np.zeros_like(x + y)
     )
     b = (
-        ((x + 3 * y) % weights["b"]) * levels["b"]
-        if weights["b"]
+        ((x_factors["b"] * x + y_factors["b"] * y) % velocitys["b"])
+        * (levels["b"] * ((256 / velocitys["b"]) if not blink else 1))
+        if velocitys["b"]
         else np.zeros_like(x + y)
     )
     return np.stack([r, g, b], axis=-1).astype(np.uint8)
@@ -113,7 +134,7 @@ def save_mp4(
 
 def save_gif(paths: Iterable[str]):
     print("saving gif")
-    first = Image.open(paths[0])
+    first = Image.open(list(paths)[0])
     rest = (Image.open(f) for f in paths)
     first.save("result.gif", save_all=True, append_images=rest, duration=30, loop=0)
 
@@ -124,22 +145,36 @@ template = Template(
     initial_r=255,
     initial_g=1,
     initial_b=100,
-    velocity_r=-0.5,
+    velocity_r=-0.6,
     velocity_g=0.7,
-    velocity_b=-1,
-    level_g=0.6,
+    velocity_b=-0.75,
+    level_r=0.9,
+    level_g=0.5,
+    level_b=0.9,
+    level_change_r=1,
+    level_change_g=1,
+    level_change_b=1,
+    x_factor_r=5,
+    x_factor_g=0.3,
+    x_factor_b=300,
+    y_factor_r=2,
+    y_factor_g=0,
+    y_factor_b=300,
+    flat_g=0.5,
     turn_back_r=True,
     turn_back_g=True,
     turn_back_b=True,
 )
 t1 = time.time()
-params = get_parameters(200, template)
+params = get_parameters(1000, template)
 patterns = (
     create_pattern(
         image_params["width"],
         image_params["height"],
         image_params["weights"],
         image_params["levels"],
+        image_params["x_factors"],
+        image_params["y_factors"],
     )
     for image_params in params
 )
